36ea156f67614b994d651fdb6ca4ea6a8fd228d8
faisalmamora749 <faisalmamora749@gmail.com>
2025-10-27 10:59:48 +0700

    update 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,15 @@ import { Routes } from '@angular/router';
 import { Dashboard } from './dashboard/dashboard';
 import { Login } from './login/login';
 import { Admin } from './admin/admin';
+import { Dashboard2 } from './dashboard2/dashboard2';
+import { Dashboard3 } from './dashboard3/dashboard3';
 
 
 export const routes: Routes = [
     { path: "", redirectTo: "login", pathMatch: "full" },
     { path:"admin", component: Admin},
     { path: "dashboard", component: Dashboard },
+    { path: "dashboard", component: Dashboard2 },
+    { path: "dashboard", component: Dashboard3 },
     { path: "login", component: Login },
 ];
--- src/app/dashboard3/dashboard3.ts
+++ src/app/dashboard3/dashboard3.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { Header } from "../header/header";
+import { Sidebar } from "../sidebar/sidebar";
+import { Footer } from "../footer/footer";
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard3',
-  imports: [],
+  imports: [RouterModule, Header, Sidebar, Footer],
   templateUrl: './dashboard3.html',
   styleUrl: './dashboard3.css'
 })
